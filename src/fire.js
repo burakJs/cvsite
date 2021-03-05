cc0798effa106a2f1a76fd014eb993d2ccb58a93
Burak İmdat <70351342+burakJs@users.noreply.github.com>
2021-03-05 22:56:14 +0300

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyArl0azgjYH89GuEEhC10UxUR4P2XIMg74",
-  authDomain: "cvsite-b317d.firebaseapp.com",
-  projectId: "cvsite-b317d",
-  storageBucket: "cvsite-b317d.appspot.com",
-  messagingSenderId: "577080235948",
-  appId: "1:577080235948:web:d91c1d9993e51560c78c09"
+  apiKey: "******************************",
+  authDomain: "******************************",
+  projectId: "******************************",
+  storageBucket: "******************************",
+  messagingSenderId: "******************************",
+  appId: "******************************"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
